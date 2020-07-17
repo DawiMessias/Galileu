14508d6f38ab7eec834003a25da0600ba840d02d
david <wtih123@gmail.com>
2020-07-16 22:38:26 -0300

    route adjustment and file import

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const express = require("express")
-
 const routes = express.Router()
-
+const teachers = require("./teachers")
 
 routes.get("/", function(req, res) {
     return res.render("layout")
@@ -11,9 +10,7 @@ routes.get("/teachers", function(req, res) {
     return res.render("teachers/index")
 })
 
-routes.post("/teachers", function(req, res) {
-    return res.send("OKEI")
-})
+routes.post("/teachers", teachers.post)
 
 routes.get("/teachers/create", function(req, res) {
     return res.render("teachers/create")
